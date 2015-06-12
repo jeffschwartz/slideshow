d01e250129fb27746bf3739abea9314862ab649c
Jeff Schwartz <jefftschwartz@gmail.com>
2015-06-12 12:33:07 -0400

    Add 1 more image file. Flattened out the DOM - control is now just an 'img' tag.

--- javascript/imageviewer.js
+++ javascript/imageviewer.js
@@ -1,97 +1,86 @@
 (function(window, undefined){
     'use strict';
-    var imageviewerEl = document.getElementById('imageviewer'),
-        basePath = imageviewerEl.getAttribute('data-base-path') || 'images/',
-        images = imageviewerEl.getAttribute('data-images').split(','),
-        outerBgColor = imageviewerEl.getAttribute('data-outer-bg-color'),
-        innerBgColor = imageviewerEl.getAttribute('data-inner-bg-color'),
-        delay = imageviewerEl.getAttribute('data-delay') || 500,
-        outerContainerEl = document.createElement('div'),
-        innerContainerEl = document.createElement('div'),
-        imageEl = document.createElement('img'),
-        width = imageviewerEl.getAttribute('data-width'),
-        height = imageviewerEl.getAttribute('data-height'),
-        index;
+    var imgs = [],
+        els = [];
 
-    function addStylesToEl(el, elStyles){
-        var st = el.style;
-        elStyles.forEach(function(style){
-            st[style[0]] = style[1];
-        });
+    function imageViwer(el){
+        var basePath = el.getAttribute('data-base-path') || 'images/',
+            images = el.getAttribute('data-images').split(','),
+            delay = el.getAttribute('data-delay') || 500,
+            bp = '',
+            index;
+
+        function addStylesToEl(el, elStyles){
+            var st = el.style;
+            elStyles.forEach(function(style){
+                st[style[0]] = style[1];
+            });
+        }
+
+        function fadeIn(){
+            var opacity = 0,
+                tid;
+            el.setAttribute('src', images[index]);
+            tid = setInterval(function(){
+                opacity += 0.01;
+                addStylesToEl(el, [['opacity', opacity]]);
+                if(opacity >= 1){
+                    clearInterval(tid);
+                    pause(el);
+                }
+            }, 10);
+        }
+
+        function pause(){
+           setTimeout(function(){
+              fadeOut(el);
+           }, delay);
+        }
+
+        function fadeOut(){
+            var opacity = 1,
+                tid;
+            tid = setInterval(function(){
+                opacity -= 0.01;
+                addStylesToEl(el, [['opacity', opacity]]);
+                if(opacity <= 0){
+                    clearInterval(tid);
+                    index += 1;
+                    index = index > images.length - 1 ? 0 : index;
+                    fadeIn(el);
+                }
+            }, 10);
+        }
+
+        bp = basePath[basePath.length - 1];
+        bp = bp === '/' ? basePath : basePath + '/';
+
+        //Add basepath to each image
+        images = images.map(function(i){
+            var img = i.trim();
+            return bp + img;
+        })  ;
+
+        //Show the 1st image
+        index = 0;
+        el.setAttribute('src', images[index]);
+
+        //... and pause
+        setTimeout(function(){
+            pause();
+        }, 1);
+
     }
 
-    function fadeIn(el){
-        var opacity = 0,
-            tid;
-        imageEl.setAttribute('src', images[index]);
-        tid = setInterval(function(){
-            opacity += 0.01;
-            addStylesToEl(el, [['opacity', opacity]]);
-            if(opacity >= 1){
-                clearInterval(tid);
-                pause(el);
-            }
-        }, 10);
-    }
-
-    function pause(el){
-       setTimeout(function(){
-          fadeOut(el);
-       }, delay);
-    }
-
-    function fadeOut(el){
-        var opacity = 1,
-            tid;
-        tid = setInterval(function(){
-            opacity -= 0.01;
-            addStylesToEl(el, [['opacity', opacity]]);
-            if(opacity <= 0){
-                clearInterval(tid);
-                index += 1;
-                index = index > images.length - 1 ? 0 : index;
-                fadeIn(el);
-            }
-        }, 10);
-    }
-
-    //Set style attributes
-    outerContainerEl.setAttribute('class', 'imageviewer-outer-container');
-    innerContainerEl.setAttribute('class', 'imageviewer-inner-container');
-    imageEl.setAttribute('class', 'imageviewer-img');
-    innerContainerEl.appendChild(imageEl);
-    outerContainerEl.appendChild(innerContainerEl);
-
-    //Override style attributes
-    if(outerBgColor){
-        addStylesToEl(outerContainerEl, [['background-color', outerBgColor]]);
-    }
-    if(innerBgColor){
-        addStylesToEl(innerContainerEl, [['background-color', innerBgColor]]);
-    }
-    if(width){
-        addStylesToEl(innerContainerEl, [['width', width]]);
-    }
-    if(height){
-        addStylesToEl(innerContainerEl, [['height', height]]);
-    }
-
-    //Add outer container to the DOM
-    imageviewerEl.appendChild(outerContainerEl);
-
-    //Add basepath to each image
-    images = images.map(function(i){
-        var img = i.trim();
-        return basePath + img;
+    //Get all images elements
+    imgs = document.getElementsByTagName('img');
+    imgs = [].slice.call(imgs, 0);
+    //Filter for intended image viewers
+    els = imgs.filter(function(img){
+        return !!img.getAttribute('data-image-viewer');
     });
-
-    //Show the 1st image
-    index = 0;
-    imageEl.setAttribute('src', images[index]);
-
-    //... and pause
-    setTimeout(function(){
-        pause(imageEl);
-    }, 1);
-
+    //Turn them into image viewers
+    els.forEach(function(img){
+        imageViwer(img);
+    });
 }(window));
